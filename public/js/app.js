7133dc212afbc0e062195e25cdd42b0830e8a34c
David Walton <davidwalton6772@gmail.com>
2020-11-04 14:55:20 -0600

    tailwind.css

--- public/js/app.js
+++ public/js/app.js
@@ -49716,6 +49716,17 @@ module.exports = function(module) {
 
 /***/ }),
 
+/***/ "./resources/css/main.css":
+/*!********************************!*\
+  !*** ./resources/css/main.css ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ "./resources/js/app.js":
 /*!*****************************!*\
   !*** ./resources/js/app.js ***!
@@ -49880,14 +49891,15 @@ __webpack_require__.r(__webpack_exports__);
 /***/ }),
 
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!**************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/css/main.css ***!
+  \**************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /Users/davidwalton/Code/test_projects/tweety/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/davidwalton/Code/test_projects/tweety/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/davidwalton/Code/test_projects/tweety/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /Users/davidwalton/Code/test_projects/tweety/resources/css/main.css */"./resources/css/main.css");
 
 
 /***/ })
